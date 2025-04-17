51fdc06329de23ad01fc6b54ae0fd9079c6a1213
DeaHyun0911 <eogus0911@gmail.com>
2025-04-17 14:19:17 +0900

    test: 회원가입 저장소 테스트

---
+++ src/test/java/com/jwt/repository/AuthLocalRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.jwt.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.jwt.domain.User;
+
+class AuthLocalRepositoryTest {
+
+	AuthLocalRepository authLocalRepository = new AuthLocalRepository();
+
+	@Test
+	public void save() {
+		//given
+		User user = User.builder()
+			.username("test")
+			.password("1234")
+			.nickname("nickname")
+			.build();
+
+		//when
+		authLocalRepository.save(user);
+		User result = authLocalRepository.findByUsername(user.getUsername()).get();
+
+		//then
+		assertThat(user).isEqualTo(result);
+	}
+
+	@Test
+	public void findByUsername() {
+		//given
+		User user = User.builder()
+			.username("test1")
+			.password("1234")
+			.nickname("nickname")
+			.build();
+		authLocalRepository.save(user);
+
+		//when
+		User result = authLocalRepository.findByUsername(user.getUsername()).get();
+
+		//then
+		assertThat(user).isEqualTo(result);
+	}
+
+}
