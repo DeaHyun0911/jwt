fd06c0e6a74720a9e230523ff33c74727d94bd1a
DeaHyun0911 <eogus0911@gmail.com>
2025-04-17 16:21:10 +0900

    feat: 비밀번호 자리수 검증 추가

--- src/main/java/com/jwt/dto/request/SignupRequest.java
+++ src/main/java/com/jwt/dto/request/SignupRequest.java
@@ -1,6 +1,7 @@
 package com.jwt.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,6 +13,7 @@ public class SignupRequest {
 	private final String username;
 
 	@NotBlank(message = "비밀번호를 입력해주세요.")
+	@Size(min = 4, message = "비밀번호는 4자리 이상이여야 합니다.")
 	private final String password;
 
 	@NotBlank(message = "닉네임을 입력해주세요.")
