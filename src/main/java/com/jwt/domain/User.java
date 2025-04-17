8f8fa1d104d38a148392f3e129b186accf5e908e
DeaHyun0911 <eogus0911@gmail.com>
2025-04-17 13:52:19 +0900

    feat: 회원 생성자 추가

--- src/main/java/com/jwt/domain/User.java
+++ src/main/java/com/jwt/domain/User.java
@@ -1,14 +1,19 @@
 package com.jwt.domain;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class User {
-	private final Long id;
 	private final String username;
 	private final String password;
 	private final String nickname;
 	private final UserRole role = UserRole.USER;
+
+	@Builder
+	public User(String username, String password, String nickname) {
+		this.username = username;
+		this.password = password;
+		this.nickname = nickname;
+	}
 }
