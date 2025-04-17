31997b74ecf240856b84e13999dfe8860edba32c
DeaHyun0911 <eogus0911@gmail.com>
2025-04-17 13:51:59 +0900

    feat: 회원가입 응답 정적메서드 추가

--- src/main/java/com/jwt/dto/response/SignupResponse.java
+++ src/main/java/com/jwt/dto/response/SignupResponse.java
@@ -1,5 +1,6 @@
 package com.jwt.dto.response;
 
+import com.jwt.domain.User;
 import com.jwt.domain.UserRole;
 
 import lombok.AllArgsConstructor;
@@ -11,4 +12,8 @@ public class SignupResponse {
 	private final String username;
 	private final String nickname;
 	private final UserRole userRole;
+
+	public static SignupResponse from(User user) {
+		return new SignupResponse(user.getUsername(), user.getNickname(), user.getRole());
+	}
 }
