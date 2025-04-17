444bbca8c67432e79c638795c3b58232ba165e24
DeaHyun0911 <eogus0911@gmail.com>
2025-04-18 01:47:36 +0900

    feat: 스프링 시큐리티 설정 추가

---
+++ src/main/java/com/jwt/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.jwt.filter.JwtAuthFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	public static final String[] AUTH_ALLOWLIST = {
+		"/auth/signup",
+		"/auth/login",
+		"/swagger-ui/**",
+		"/v3/api-docs"
+	};
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf((csrf) -> csrf.disable())
+			.cors(((Customizer.withDefaults())))
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.formLogin((form) -> form.disable())
+
+			.addFilterBefore(new JwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(AUTH_ALLOWLIST).permitAll()
+				.anyRequest().authenticated());
+
+		return http.build();
+
+	}
+}
