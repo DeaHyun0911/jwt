fea22139c654c8027c00565ec126780bcdb166f6
DeaHyun0911 <eogus0911@gmail.com>
2025-04-18 20:31:54 +0900

    feat: 실제 유저의 토큰정보를 저장하도록 수정

--- src/main/java/com/jwt/filter/JwtAuthFilter.java
+++ src/main/java/com/jwt/filter/JwtAuthFilter.java
@@ -4,10 +4,13 @@ import static com.jwt.exception.ErrorCode.*;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -17,6 +20,8 @@ import com.jwt.exception.CommonException;
 import com.jwt.util.JwtUtil;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,37 +35,52 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 									FilterChain filterChain) throws ServletException, IOException {
 
-		String requestURI = request.getRequestURI();
-		if(isAllowListed(requestURI)) {
+		// JWT 토큰 검증 예외 경로는 다음 필터로 넘김
+		if(isAllowListed(request)) {
 			filterChain.doFilter(request, response);
 			return;
 		}
 
 		String accessToken = extractToken(request);
+		System.out.println("accessToken = " + accessToken);
 
-		Claims claims = JwtUtil.parseToken(accessToken);
-
-		String userName = claims.getSubject();
-		String nickName = claims.get("nickName", String.class);
-		String role = claims.get("role", String.class);
-
-		System.out.println("userName = " + userName);
-		System.out.println("nickName = " + nickName);
-		System.out.println("role = " + role);
-
-		Authentication authentication = new TestingAuthenticationToken("username", "password", "ROLE_TEST");
+		Authentication authentication = createAuthentication(accessToken);
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-
 
 		filterChain.doFilter(request, response);
 	}
 
-	private static boolean isAllowListed(String requestURI) {
+	private static Authentication createAuthentication(String accessToken) {
+		try {
+			Claims claims = JwtUtil.parseToken(accessToken);
+
+			String userName = claims.getSubject();
+			String role = claims.get("roles", String.class);
+
+			System.out.println("role = " + role);
+
+			List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE_" + role));
+			System.out.println("✅ 권한 확인: " + authorities);
+
+			Authentication authentication = new TestingAuthenticationToken(userName, null, authorities);
+			return authentication;
+
+		} catch (ExpiredJwtException e) {
+			throw new CommonException(EXPIRED_TOKEN);
+		} catch (JwtException e) {
+			throw new CommonException(INVALID_TOKEN);
+		}
+	}
+
+	private static boolean isAllowListed(HttpServletRequest request) {
+		String requestURI = request.getRequestURI();
 		return Arrays.stream(SecurityConfig.AUTH_ALLOWLIST).anyMatch(url -> antPathMatcher.match(url, requestURI));
 	}
 
 	private String extractToken(HttpServletRequest request) {
 		String authorizationHeader = request.getHeader("Authorization");
+		System.out.println("authorizationHeader = '" + authorizationHeader + "'");
+
 
 		if(StringUtils.isNotBlank(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
 			return authorizationHeader.substring(7);
